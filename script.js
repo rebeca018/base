afbd96b08e7d6db00e38f246f4627cece7f5d44e
Rebeca <rebecasnascimento018@gmail.com>
2023-05-05 08:00:44 -0400

    Commit estilo CSS

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ class Produto{
     }
     mostrar_produto(){
         return `
-            <div>${this.nome}</div>
-            <div>${this.data_cadastro}</div>
-            <div>${this.discricao}</div>
-            <div>${this.preco}</div>
-
+            <div class="book-card">
+            <div class="book-card-h4">${this.nome}</div>
+            <div class="book-card-p">${this.data_cadastro}</div>
+            <div class="book-card-p">${this.descricao}</div>
+            <div class="book-card-p">${this.preco}</div>
+            </div>
         `;
         //return this.nome +  this.data_cadastro + this.descricao + this.preco;
     }
@@ -26,11 +27,13 @@ class ProdutoDestaque extends Produto{
     }
     mostrar_produto_destaque(){
         return `
-            <div>${this.nome}</div>
-            <div>${this.data_cadastro}</div>
-            <div>${this.discrição}</div>
-            <div>${this.preco}</div>
-            <div>${this.imagem_produtoD}</div>
+            <div class="book-card">
+            <img src="${this.imagem_produtoD}" class="book-card-img">
+            <div class="book-card-h4">${this.nome}</div>
+            <div class="book-card-p">${this.data_cadastro}</div>
+            <div class="book-card-p">${this.descricao}</div>
+            <div class="book-card-p">${this.preco}</div>
+            </div>
 
         `;
         
@@ -44,14 +47,22 @@ const div = document.getElementById('produto-destaque');
 div.insertAdjacentHTML('afterbegin', produto.mostrar_produto_destaque());
 
 
-const produto2 = new Produto("Legend", "03/05/2023", "Livro", 18.05);
-console.log(produto2.mostrar_produto());
+const produto2 = new ProdutoDestaque("Legend", "03/05/2023", "Livro", 18.05, "https://m.media-amazon.com/images/I/A13bJYSllML._AC_UL480_FMwebp_QL65_.jpg");
+console.log(produto2.mostrar_produto_destaque());
+const div2 = document.getElementById('produto-destaque');
+div2.insertAdjacentHTML('afterbegin', produto2.mostrar_produto_destaque());
+
 
 const produto3 = new ProdutoDestaque("Six of Crows", "03/05/2023", "Livro", 29.99, "https://m.media-amazon.com/images/I/51ku0tEgIcL._SY346_.jpg");
 console.log(produto3.mostrar_produto_destaque());
+const div3 = document.getElementById('produto-destaque');
+div3.insertAdjacentHTML('afterbegin', produto3.mostrar_produto_destaque());
 
-const produto4 = new Produto("Jogos de Herança", "03/05/2023", "Livro", 37.92);
-console.log(produto4.mostrar_produto());
+
+const produto4 = new ProdutoDestaque("Jogos de Herança", "03/05/2023", "Livro", 37.92, "https://m.media-amazon.com/images/I/81e98vyHFmS._AC_UL480_FMwebp_QL65_.jpg");
+console.log(produto4.mostrar_produto_destaque());
+const div4 = document.getElementById('produto-destaque');
+div4.insertAdjacentHTML('afterbegin', produto4.mostrar_produto_destaque());
 
 
 
